05e27cb01e03399f6a6b25917404b77772fe00b6
DESKTOP-M80U7G1\Harry <h7719201@gmail.com>
2021-01-05 19:08:19 +0800

    文章功能-start

---
+++ ithelp/resources/views/layouts/article.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}">
+    <title>Harry 幫幫忙!</title>
+</head>
+<body>
+    <main css="m-4">
+        @yield('main')
+    </main>
+</body>
+<script src="{{ asset('js/app.js') }}"></script>
+</html>
